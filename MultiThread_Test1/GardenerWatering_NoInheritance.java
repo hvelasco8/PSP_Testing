f81c1569f5d18f29f5bfd251d84823f8144a8ba1
hvelasco8 <166366156+hvelasco8@users.noreply.github.com>
2025-01-20 08:35:21 +0100

    Merge GardenerWatering_NoInheritance.java + MultiThreadGardenerWatering_NoInheritance.java
    Added the code from MultiThreadGardenerWatering_NoInheritance into GardenerWatering_NoInheritance

--- MultiThread_Test1/GardenerWatering_NoInheritance.java
+++ MultiThread_Test1/GardenerWatering_NoInheritance.java
@@ -3,22 +3,38 @@ package testing1;
 public class GardenerWatering_NoInheritance implements Runnable {
 	private String gardenerName;
 	private int wateringTime;
-	
+
 	public GardenerWatering_NoInheritance(String gardenerName, int wateringTime) {
 		super();
 		this.gardenerName = gardenerName;
 		this.wateringTime = wateringTime;
 	}
-	
+
 	@Override
 	public void run() {
 		System.out.println(gardenerName + " started watering.");
 		try {
 			Thread.sleep(wateringTime*1000L);
-		} catch (InterruptedException e){
+		} catch (InterruptedException e) {
 			System.out.println(gardenerName + " was interrupted while watering.");
 		}
 		System.out.println(gardenerName + " finished watering.");
+	}
+
+	public static void main (String[] args) {
+		GardenerWatering_NoInheritance  gardener1 = new GardenerWatering_NoInheritance("Dahlia the gardener", 3);
+		GardenerWatering_NoInheritance gardener2 = new GardenerWatering_NoInheritance("Daisy the gardener", 2);
+		GardenerWatering_NoInheritance gardener3 = new GardenerWatering_NoInheritance("Orchid the gardener", 1);
+
+		Thread thread1 = new Thread(gardener1);
+		Thread thread2 = new Thread(gardener2);
+		Thread thread3 = new Thread(gardener3);
+
+		thread1.start();
+		thread2.start();
+		thread3.start();
+
+		System.out.println("All gardeners are gardening...");
 	}
 	
 }
